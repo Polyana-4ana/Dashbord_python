a6a506c2a3531a028b97aa61df395d11dfe3c802
Polyana Sanhudo <polinhasantos74@gmail.com>
2026-01-22 15:14:58 -0300

    corrigindo erros do dataset.py

--- dataset.py
+++ dataset.py
@@ -1,15 +1,16 @@
 import json
 import pandas as pd
 
-# lendo o arquivo json corretamente
+# lendo o arquivo json
 with open('dados/vendas.json', encoding='utf-8') as file:
     data = json.load(file)
 
 # criando dataframe
-df = pd.DataFrame.from_dict(data)
+df = pd.DataFrame(data)
 
-# convertendo a coluna de data
+# convertendo datas de forma segura
 df['Data da Compra'] = pd.to_datetime(
     df['Data da Compra'],
-    format='%d/%m/%y'
+    dayfirst=True,
+    errors='coerce'
 )
